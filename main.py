99e6bb23d16dcf91d66b8140981394849c4901c2
Suraj Kumar Dev <blackmesd@gmail.com>
2024-06-07 05:32:44 +0530

    main.py which has the main code

---
+++ main.py
@@ -0,0 +1,13 @@
+#first install pytwhatkit by typing in your terminal
+#pip install pywhatkit then write the following line of code
+
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+918986536936","Write the message in these double quotes ",19,43)
+
+#Here the sednwhatmsg has three parameters as 
+#1.Mobile Number whom you wnat to send message
+#2.The message you want to send 
+#3.The time you want the message to be sent 
+#4.Here the 19 represents the 7 o clock and 43 represents the minute
